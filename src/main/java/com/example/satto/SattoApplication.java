1f2116ce317779401577de0e2dddc1f6e246339c
rlJzr <113494060+rlJzr@users.noreply.github.com>
2024-04-05 22:42:16 +0900

    :sparkles: feat : 프로젝트 생성

---
+++ src/main/java/com/example/satto/SattoApplication.java
@@ -0,0 +1,13 @@
+package com.example.satto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SattoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SattoApplication.class, args);
+	}
+
+}
