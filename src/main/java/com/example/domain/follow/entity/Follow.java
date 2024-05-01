4bd33d3110768b40add07cea46afafd7dbb968f9
HyunwooYi <83715335+HyunwooYi@users.noreply.github.com>
2024-05-02 02:36:24 +0900

    유저, 팔로우 도메인 작성

---
+++ src/main/java/com/example/domain/follow/entity/Follow.java
@@ -0,0 +1,25 @@
+package com.example.domain.follow.entity;
+
+import com.example.domain.user.entity.User;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+public class Follow {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "follow_id")
+    private Long followId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "followed_id")
+    private User followedId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "follower_id")
+    private User followerId;
+
+    @Column(name = "follow_date")
+    private LocalDate followDate;
+}
