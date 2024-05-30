b0a91eec1849f56cc6d116897bbb033a01d36b93
rlJzr <eopy0928@naver.com>
2024-05-31 07:12:35 +0900

    ✨ Feat: 현재 학기 강의 조회 기능 추가

--- src/main/java/com/example/satto/domain/currentLecture/controller/CurrentLectureController.java
+++ src/main/java/com/example/satto/domain/currentLecture/controller/CurrentLectureController.java
@@ -1,15 +1,27 @@
 package com.example.satto.domain.currentLecture.controller;
 
-
+import com.example.satto.domain.currentLecture.dto.CurrentLectureResponseDTO;
+import com.example.satto.domain.currentLecture.service.CurrentLectureService;
+import com.example.satto.global.common.BaseResponse;
 import lombok.RequiredArgsConstructor;
-
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/lecture")
 public class CurrentLectureController {
 
+    private final CurrentLectureService currentLectureService;
+
+
+    //TODO: QueryDSL을 이용한 강의 필터링 기능 추가 구현 필요
+    @GetMapping("/search")
+    public BaseResponse<List<CurrentLectureResponseDTO>> getCurrentLecture(){
+        List<CurrentLectureResponseDTO> result =  currentLectureService.getCurrentLecture();
+        return BaseResponse.onSuccess(result);
+    }
 }
--- src/main/java/com/example/satto/domain/currentLecture/service/CurrentLectureService.java
+++ src/main/java/com/example/satto/domain/currentLecture/service/CurrentLectureService.java
@@ -1,11 +1,23 @@
 package com.example.satto.domain.currentLecture.service;
 
-
+import com.example.satto.domain.currentLecture.dto.CurrentLectureResponseDTO;
+import com.example.satto.domain.currentLecture.entity.CurrentLecture;
+import com.example.satto.domain.currentLecture.repository.CurrentLectureRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class CurrentLectureService {
 
+    private final CurrentLectureRepository currentLectureRepository;
+
+    public List<CurrentLectureResponseDTO> getCurrentLecture(){
+        List<CurrentLecture> allLecture = currentLectureRepository.findAll();
+        List<CurrentLectureResponseDTO> currentLectureResponseDTOList = CurrentLectureResponseDTO.from(allLecture);
+        return currentLectureResponseDTOList;
+    }
 }
