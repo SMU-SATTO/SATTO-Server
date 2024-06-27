169aaf91f5b3a96b55c4e4b3fc0fc9ddaff8cb08
rlJzr <eopy0928@naver.com>
2024-06-27 10:51:13 +0900

    refactor : DTO 수정

--- src/main/java/com/example/satto/domain/timeTable/dto/TimeTableResponseDTO.java
+++ src/main/java/com/example/satto/domain/timeTable/dto/TimeTableResponseDTO.java
@@ -1,7 +1,9 @@
 package com.example.satto.domain.timeTable.dto;
 
 import com.example.satto.domain.currentLecture.dto.CurrentLectureResponseDTO;
+import com.example.satto.domain.currentLecture.entity.CurrentLecture;
 import com.example.satto.domain.timeTable.entity.TimeTable;
+import lombok.Builder;
 
 import java.util.List;
 import java.util.Set;
@@ -9,27 +11,29 @@ import java.util.stream.Collectors;
 
 public record TimeTableResponseDTO(
 ) {
+
+    @Builder
     public static record EntireTimeTableResponseDTO(
-            List<CurrentLectureResponseDTO> timeTable,
+            List<TimeTableResponseDTO.TimeTableLectureDTO> timeTable,
             String totalTime
     ) {
-        public static String calculateTotalTime(List<CurrentLectureResponseDTO> timetable) {
+        public static String calculateTotalTime(List<TimeTableResponseDTO.TimeTableLectureDTO> timetable) {
             String tt = "";
-            for (CurrentLectureResponseDTO lecture : timetable) {
+            for (TimeTableResponseDTO.TimeTableLectureDTO lecture : timetable) {
                 tt += lecture.lectTime();
             }
             return tt;
         }
 
 
-        public static TimeTableResponseDTO.EntireTimeTableResponseDTO from(List<CurrentLectureResponseDTO> timeTable){
-            return new TimeTableResponseDTO.EntireTimeTableResponseDTO(
-                    timeTable,
-                    calculateTotalTime(timeTable)
-            );
+        public static TimeTableResponseDTO.EntireTimeTableResponseDTO from(List<TimeTableResponseDTO.TimeTableLectureDTO> timeTable){
+            return EntireTimeTableResponseDTO.builder()
+                    .timeTable(timeTable)
+                    .totalTime(calculateTotalTime(timeTable))
+                    .build();
         }
 
-        public static List<TimeTableResponseDTO.EntireTimeTableResponseDTO> fromList(List<List<CurrentLectureResponseDTO>> timeTableList){
+        public static List<TimeTableResponseDTO.EntireTimeTableResponseDTO> fromList(List<List<TimeTableResponseDTO.TimeTableLectureDTO>> timeTableList){
             return
                     timeTableList.stream()
                             .map(TimeTableResponseDTO.EntireTimeTableResponseDTO::from)
@@ -37,6 +41,7 @@ public record TimeTableResponseDTO(
         }
     }
 
+    @Builder
     public static record timeTableListDTO(
             Long timeTableId,
             String semesterYear,
@@ -45,13 +50,13 @@ public record TimeTableResponseDTO(
             boolean isRepresent
     ){
         public static TimeTableResponseDTO.timeTableListDTO from(TimeTable timeTable){
-            return new TimeTableResponseDTO.timeTableListDTO(
-                    timeTable.getTimetableId(),
-                    timeTable.getSemesterYear(),
-                    timeTable.getTimetableName(),
-                    timeTable.getIsPublic(),
-                    timeTable.getIsRepresented()
-            );
+            return TimeTableResponseDTO.timeTableListDTO.builder()
+                    .timeTableId(timeTable.getTimetableId())
+                    .semesterYear(timeTable.getSemesterYear())
+                    .timeTableName(timeTable.getTimetableName())
+                    .isPublic(timeTable.getIsPublic())
+                    .isRepresent(timeTable.getIsRepresented())
+                    .build();
         }
 
         public static List<TimeTableResponseDTO.timeTableListDTO> fromList(List<TimeTable> timeTableList){
@@ -68,24 +73,26 @@ public record TimeTableResponseDTO(
     ) {
     }
 
+    @Builder
     public static record SelectTimeTableResponseDTO(
             Long timeTableId,
-            List<CurrentLectureResponseDTO> lects,
+            List<TimeTableResponseDTO.TimeTableLectureDTO> lects,
             String semesterYear,
             String timeTableName,
             boolean isPublic,
             boolean isRepresented
 
     ){
-        public static TimeTableResponseDTO.SelectTimeTableResponseDTO from(List<CurrentLectureResponseDTO> lects, TimeTable timeTable){
-            return new TimeTableResponseDTO.SelectTimeTableResponseDTO(
-                    timeTable.getTimetableId(),
-                    lects,
-                    timeTable.getSemesterYear(),
-                    timeTable.getTimetableName(),
-                    timeTable.getIsPublic(),
-                    timeTable.getIsRepresented()
-            );
+        public static TimeTableResponseDTO.SelectTimeTableResponseDTO from(List<TimeTableResponseDTO.TimeTableLectureDTO> lects, TimeTable timeTable){
+
+            return SelectTimeTableResponseDTO.builder()
+                    .timeTableId(timeTable.getTimetableId())
+                    .lects(lects)
+                    .semesterYear(timeTable.getSemesterYear())
+                    .timeTableName(builder().timeTableName)
+                    .isPublic(timeTable.getIsPublic())
+                    .isRepresented(timeTable.getIsRepresented())
+                    .build();
         }
 
     }
@@ -94,6 +101,36 @@ public record TimeTableResponseDTO(
             String lectName,
             String code
     ){}
+
+    @Builder
+    public static record TimeTableLectureDTO(
+            String code,
+            String codeSection,
+            String lectName,
+            String professor,
+            String lectTime,
+            String cmpDiv,
+            int credit
+    ){
+        public static TimeTableResponseDTO.TimeTableLectureDTO from(CurrentLecture currentLecture){
+            return TimeTableLectureDTO.builder()
+                    .code(currentLecture.getCode())
+                    .codeSection(currentLecture.getCodeSection())
+                    .lectName(currentLecture.getLectName())
+                    .professor(currentLecture.getProfessor())
+                    .lectTime(currentLecture.getLectTime())
+                    .cmpDiv(currentLecture.getCmpDiv())
+                    .credit(currentLecture.getCredit())
+                    .build();
+        }
+
+        public static List<TimeTableResponseDTO.TimeTableLectureDTO> fromList(List<CurrentLecture> currentLectures){
+            return
+                    currentLectures.stream()
+                            .map(TimeTableResponseDTO.TimeTableLectureDTO::from)
+                            .collect(Collectors.toList());
+        }
+    }
 
 
 }
